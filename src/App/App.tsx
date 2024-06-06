c81db09a5cee9034bea1a1d7cda790e4cb6cdcdb
Kristina <kristina.dudnyk20@gmail.com>
2024-06-06 12:38:00 +0100

    code refactoring

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
-import AvatarList from "./components/AvatarList/index";
-import AvatarEditor from "./components/AvatarEditor/index";
-import Footer from "./components/Footer";
 import domtoimage from "dom-to-image";
 import { saveAs } from "file-saver";
 import ReactNiceAvatar, { genConfig } from "./config/index";
+import AvatarList from "./components/AvatarList";
+import AvatarEditor from "./components/AvatarEditor";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
-import "./index.scss";
 import Form from "./components/Form";
 import Arrow from "./components/Arrow";
 import AboutUs from "./components/AboutUs";
+import "./index.scss";
 
 interface AppState {
   config: { [key: string]: any };
@@ -25,40 +25,38 @@ class App extends Component<{}, AppState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      config: genConfig({
-        isGradient: Boolean(Math.round(Math.random())),
-      }),
+      config: genConfig({ isGradient: Boolean(Math.round(Math.random())) }),
       shape: "circle",
       avatarId: "myAvatar",
       avatarImageDataUrl: null,
     };
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    this.captureAvatarImage = this.captureAvatarImage.bind(this);
   }
 
-  selectConfig(config: { [key: string]: any }) {
+  // Select configuration for avatar
+  selectConfig = (config: { [key: string]: any }) => {
     this.setState({ config });
-  }
+  };
 
-  updateConfig(key: string, value: any) {
-    const { config } = this.state;
-    config[key] = value;
-    this.setState({ config });
-  }
+  // Update specific configuration key
+  updateConfig = (key: string, value: any) => {
+    this.setState((prevState) => ({
+      config: { ...prevState.config, [key]: value },
+    }));
+  };
 
-  updateShape(shape: AvatarShape) {
+  // Update the shape of the avatar
+  updateShape = (shape: AvatarShape) => {
     this.setState({ shape });
-  }
+  };
 
-  async download() {
+  // Download avatar as image
+  download = async () => {
     const scale = 2;
     const node = document.getElementById(this.state.avatarId);
 
     if (!node) {
       console.error(
-        "Element with ID",
-        this.state.avatarId,
-        "not found in the DOM."
+        `Element with ID ${this.state.avatarId} not found in the DOM.`
       );
       return;
     }
@@ -75,42 +73,45 @@ class App extends Component<{}, AppState> {
         width: node.offsetWidth * scale,
       });
 
-      if (blob) {
-        saveAs(blob, "avatar.png");
-      } else {
-        console.error("Blob is null or undefined.");
-      }
+      if (blob) saveAs(blob, "avatar.png");
+      else console.error("Blob is null or undefined.");
     } catch (error) {
       console.error("Error generating image blob:", error);
     }
-  }
+  };
 
-  onInputKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
-    this.setState({
-      config: genConfig(e.currentTarget.value),
-    });
-  }
+  // Handle input key up event to generate config
+  onInputKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    this.setState({ config: genConfig(e.currentTarget.value) });
+  };
 
-  async captureAvatarImage() {
+  // Capture avatar image as a data URL
+  captureAvatarImage = async () => {
     const node = document.getElementById(this.state.avatarId);
     if (node) {
-      const dataUrl = await domtoimage.toPng(node);
-      console.log(dataUrl);
-      this.setState({ avatarImageDataUrl: dataUrl }, () => {
-        console.log("State updated:", this.state.avatarImageDataUrl);
-      });
-    } else console.log("node is not exist");
-  }
+      try {
+        const dataUrl = await domtoimage.toPng(node);
+        this.setState({ avatarImageDataUrl: dataUrl });
+        console.log("State updated:", dataUrl);
+      } catch (error) {
+        console.error("Error capturing avatar image:", error);
+      }
+    } else {
+      console.log("Node does not exist");
+    }
+  };
 
-  handleFormSubmit(
+  // Handle form submission
+  handleFormSubmit = (
     formData: { name: string; region: string; role: string },
     imageDataUrl: string
-  ) {
+  ) => {
     console.log("Form submitted", formData, imageDataUrl);
-  }
+  };
 
   render() {
-    const { config, shape, avatarImageDataUrl } = this.state;
+    const { config, shape, avatarImageDataUrl, avatarId } = this.state;
+
     return (
       <div className="App flex flex-col min-h-screen overflow-x-hidden">
         <Header title="AVANART" />
@@ -121,29 +122,27 @@ class App extends Component<{}, AppState> {
               path="/"
               element={
                 <>
-                  <div id={this.state.avatarId} className="mb-10">
+                  <div id={avatarId} className="mb-10">
                     <ReactNiceAvatar
                       className="w-64 h-64 highres:w-80 highres:h-80"
                       hairColorRandom
-                      shape={this.state.shape}
+                      shape={shape}
                       {...config}
                     />
                   </div>
                   <AvatarEditor
                     config={config}
                     shape={shape}
-                    updateConfig={this.updateConfig.bind(this)}
-                    updateShape={this.updateShape.bind(this)}
-                    download={this.download.bind(this)}
+                    updateConfig={this.updateConfig}
+                    updateShape={this.updateShape}
+                    download={this.download}
                   />
                   <input
                     className="inputField w-64 h-10 p-2 rounded-full mt-10 text-center outline-none"
                     placeholder="input name or email ..."
-                    onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) =>
-                      this.onInputKeyUp(e)
-                    }
+                    onKeyUp={this.onInputKeyUp}
                   />
-                  <AvatarList selectConfig={this.selectConfig.bind(this)} />
+                  <AvatarList selectConfig={this.selectConfig} />
                   <button onClick={this.captureAvatarImage} className="mt-4">
                     Capture Avatar Image
                   </button>
